3442d329198f520d6372a9c1882f3013c55bb881
Devon Powell <devon.f.powell@gmail.com>
2023-10-19 13:34:47 -0400

    feat: add release branches

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  branches: ['main'],
+  branches: [
+    'main',
+    'release/+([0-9])?(.{+([0-9]),x}).x'
+  ],
   plugins: [
     [
       '@semantic-release/commit-analyzer',
